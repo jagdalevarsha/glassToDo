6669bc43bb61aca2bec3f4e225513b5ff7b9be17
Ashwini Khare <khare.ashwini@gmail.com>
2015-03-11 23:38:24 -0400

    Color change for quick ToDos

--- app/src/main/java/glassproject/ubicomp/com/todo/model/TaskItemListAdapter.java
+++ app/src/main/java/glassproject/ubicomp/com/todo/model/TaskItemListAdapter.java
@@ -7,9 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import glassproject.ubicomp.com.todo.R;
+
+import static glassproject.ubicomp.com.todo.R.drawable.caution_icon;
 
 public class TaskItemListAdapter extends ArrayAdapter<TaskItem> {
 
@@ -26,7 +29,8 @@ public class TaskItemListAdapter extends ArrayAdapter<TaskItem> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View rowView = inflater.inflate(R.layout.task_list_item, parent, false);
-
+        ImageView cautionView = new ImageView(getContext());
+        cautionView.setImageResource(caution_icon);
 		TaskItem taskItem = values[position];
 		
 		TextView tv = (TextView) rowView.findViewById(R.id.taskDescription);		    
@@ -36,14 +40,20 @@ public class TaskItemListAdapter extends ArrayAdapter<TaskItem> {
 		} else {
 			tv.setPaintFlags(tv.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
 		}
-
 		
 		if(taskItem.grabbed) {
 			rowView.setBackgroundColor(Color.parseColor("#77aa77"));
-            if(taskItem.isRework())rowView.setBackgroundColor(Color.parseColor("#77aa00"));
+            if(taskItem.isRework()){
+              //  rowView.addView(cautionView);
+                tv.setTextColor(Color.parseColor("#FF9036"));
+                //rowView.setBackgroundColor(Color.parseColor("#77aa00"));
+            }
 		}
         else if(taskItem.isRework()){
-            rowView.setBackgroundColor(Color.parseColor("#77aa70"));
+           // rowView.setBackgroundColor(Color.parseColor("#77aa70"));
+           // rowView.addView(cautionView);
+           // rowView.setBackgroundColor(Color.parseColor("#77aa00"));
+            tv.setTextColor(Color.parseColor("#FF9036"));
         }
 
 		return rowView;
