5f482819b73a3be48ff6b85d8ee7962a36d188ac
virtuahost <virtualghosts@gmail.com>
2015-04-30 02:40:29 -0400

    Dual Purpose speech completed

--- app/src/main/java/glassproject/ubicomp/com/todo/activity/ToDoLiveCardService.java
+++ app/src/main/java/glassproject/ubicomp/com/todo/activity/ToDoLiveCardService.java
@@ -3,17 +3,25 @@ package glassproject.ubicomp.com.todo.activity;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.widget.RemoteViews;
 
 import com.google.android.glass.timeline.LiveCard;
+import com.google.glass.logging.Log;
 import com.google.glass.voice.VoiceCommand;
 import com.google.glass.voice.VoiceConfig;
 import com.google.glass.voice.VoiceInputHelper;
 import com.google.glass.voice.VoiceListener;
 import com.google.glass.logging.FormattingLogger;
 import com.google.glass.logging.FormattingLoggers;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 import glassproject.ubicomp.com.todo.R;
 import glassproject.ubicomp.com.todo.db.TaskItemDb;
@@ -31,13 +39,14 @@ public class ToDoLiveCardService extends Service {
     private final Handler mHandler = new Handler();
     private final UpdateLiveCardRunnable mUpdateLiveCardRunnable =
             new UpdateLiveCardRunnable();
-    private static final long DELAY_MILLIS = 30000;
+    private static final long DELAY_MILLIS = 3000;
     private static final String TAG = "GlassToDo";
     private static final String KEY_TAG = "Let me wake up glass";
 
     private VoiceInputHelper mVoiceInputHelper;
     private VoiceConfig mVoiceConfig;
     public volatile boolean trigData = false;
+    private SpeechRecognizer sr;
 
     @Override
     public void onCreate() {
@@ -50,6 +59,9 @@ public class ToDoLiveCardService extends Service {
         mVoiceInputHelper = new VoiceInputHelper(this,new ToDoVoiceListener(mVoiceConfig));
         mVoiceInputHelper.setWantAudioData(true);
         mVoiceInputHelper.setVoiceConfig(mVoiceConfig);
+
+//        sr = SpeechRecognizer.createSpeechRecognizer(this);
+//        sr.setRecognitionListener(new listener());
         com.google.glass.logging.Log.v(TAG, "Started");
     }
 
@@ -62,6 +74,30 @@ public class ToDoLiveCardService extends Service {
 //        }
         // Always call setViews() to update the live card's RemoteViews.
         mLiveCard.setViews(mLiveCardView);
+    }
+
+    private void recordAudio()
+    {
+        Intent intent = new Intent(this, ToDoTaskListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
+//        sr.startListening(intent);
+
+//        try
+//        {
+//            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+//        }
+//
+//        catch (ActivityNotFoundException a)
+//        {
+//            Log.e("Speech Recogniser", a.getMessage());
+//
+//        }
     }
 
     @Override
@@ -106,6 +142,7 @@ public class ToDoLiveCardService extends Service {
             mLiveCard.unpublish();
             mLiveCard = null;
         }
+//        sr.destroy();
         super.onDestroy();
     }
 
@@ -130,13 +167,12 @@ public class ToDoLiveCardService extends Service {
                 taskItem = db.getLatestTaskItem();
 
                 // Update the remote view with the new scores.
-                if(taskItem != null && trigData)
-//                    populateTaskOnView();
-                {
-                    TaskItem createdTask = new TaskItem("Success");
-                    trigData = false;
-                    db.saveTaskItem(createdTask);
+                if(taskItem != null)
                     populateTaskOnView();
+                if(trigData)
+                {
+                    trigData = false;
+                    recordAudio();
                 }
 
                 // Queue another score update in 30 seconds.
@@ -172,7 +208,7 @@ public class ToDoLiveCardService extends Service {
 //            if(recognizedStr.contentEquals(KEY_TAG))
                 trigData = true;
 //            com.google.glass.logging.Log.v(TAG, "Recognized text: " + recognizedStr);
-            return null;
+            return voiceConfig;
         }
 
         @Override
@@ -198,4 +234,58 @@ public class ToDoLiveCardService extends Service {
 //            voiceConfig.setCustomPhrases(cusKey);
         }
     }
+
+//    private class listener implements RecognitionListener
+//    {
+//        public void onReadyForSpeech(Bundle params)
+//        {
+////                Log.d(TAG, "onReadyForSpeech");
+//        }
+//        public void onBeginningOfSpeech()
+//        {
+//            Log.d(TAG, "onBeginningOfSpeech");
+//        }
+//        public void onRmsChanged(float rmsdB)
+//        {
+////                Log.d(TAG, "onRmsChanged");
+//        }
+//        public void onBufferReceived(byte[] buffer)
+//        {
+////                Log.d(TAG, "onBufferReceived");
+//        }
+//        public void onEndOfSpeech()
+//        {
+//            Log.d(TAG, "onEndofSpeech");
+//        }
+//        public void onError(int error)
+//        {
+////                Log.d(TAG,  "error " +  error);
+//            //  mText.setText("error " + error);
+//        }
+//        public void onResults(Bundle results)
+//        {
+//            String str = new String();
+//            android.os.Debug.waitForDebugger();
+//            Log.d(TAG, "onResults " + results);
+//            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+//            str += data.get(0);
+//
+//            TaskItem createdTask = new TaskItem(str);
+//            db.saveTaskItem(createdTask);
+////                for (int i = 0; i < data.size(); i++)
+////                {
+////                    Log.d(TAG, "result " + data.get(i));
+////                    str += data.get(i);
+////                }
+//            // mText.setText("results: "+String.valueOf(data.size()));
+//        }
+//        public void onPartialResults(Bundle partialResults)
+//        {
+////                Log.d(TAG, "onPartialResults");
+//        }
+//        public void onEvent(int eventType, Bundle params)
+//        {
+////                Log.d(TAG, "onEvent " + eventType);
+//        }
+//    }
 }
